55b15a239b72926f2859c4f0b8e1773f5d8c0f9b
Fülöp Kovács <kovacs.fulop@gmail.com>
2022-10-01 00:13:31 +0200

    Switch from chroma.js to color2k

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,89 @@
-import GUI from 'lil-gui'
-import {useLayoutEffect, useState} from 'react'
-import './App.css'
-import chroma from 'chroma-js'
+import { adjustHue, parseToHsla, toHex } from "color2k";
+import GUI from "lil-gui";
+import { useLayoutEffect, useState } from "react";
+import "./App.css";
+// import color2k from "color2k";
 
-const defaultColor = '#fff'
-const gui = new GUI()
+const defaultColor = "#ffe771";
+const gui = new GUI();
 // load gui state if it was saved
-const guiStateObject = 'lil-gui-state'
+const guiStateObject = "lil-gui-state";
 gui.onFinishChange(() => {
-  localStorage.setItem(guiStateObject, JSON.stringify(gui.save()))
-})
+  localStorage.setItem(guiStateObject, JSON.stringify(gui.save()));
+});
 
-function ColorSpot({color}: {color: string}) {
+function ColorSpot({ color }: { color: string }) {
+  console.log(parseToHsla(color).at(2));
+  const [, , l] = parseToHsla(color);
+
   return (
-    <div className="color-spot" style={{background: color}}>
+    <div className="color-spot" style={{ background: color }}>
       <span
         className="color-label"
-        style={{color: chroma(color).hcl()[2] > 50 ? '#000' : '#fff'}}
+        style={{ color: l > 0.5 ? "#000" : "#fff" }}
       >
         {color}
       </span>
     </div>
-  )
+  );
 }
 
 function App() {
-  const [primaryColor, setPrimaryColor] = useState<string>(defaultColor)
+  const [primaryColor, setPrimaryColor] = useState<string>(defaultColor);
   const [complementaryColor, setComplementerColor] = useState<string>(
-    getComplementerColor(defaultColor),
-  )
+    getComplementerColor(defaultColor)
+  );
 
   function getComplementerColor(originalColor: string): string {
-    const [originalHue, s, l] = chroma(originalColor).hsl()
-    const complementaryColor = chroma.hsl(originalHue + 180, s, l).hex()
+    // const [originalHue, s, l] = chroma(originalColor).hsl();
+    const complementaryColor = adjustHue(originalColor, 180);
 
-    return complementaryColor
+    return complementaryColor;
   }
   useLayoutEffect(() => {
     // `useLayoutEffect` is executed twice
     // because of `React.StrictMode`
     if (
       !gui.children.find((x) => {
-        const prop = x as unknown as {property?: string}
-        return prop?.property === 'primary'
+        const prop = x as unknown as { property?: string };
+        return prop?.property === "primary";
       })
     ) {
       gui
-        .addColor({primary: defaultColor}, 'primary')
+        .addColor({ primary: defaultColor }, "primary")
         .onChange((newPrimaryColor: string) => {
-          setPrimaryColor(newPrimaryColor)
-          setComplementerColor(getComplementerColor(newPrimaryColor))
-        })
-      const guiObjString = localStorage.getItem(guiStateObject)
+          setPrimaryColor(newPrimaryColor);
+          setComplementerColor(getComplementerColor(newPrimaryColor));
+        });
+      const guiObjString = localStorage.getItem(guiStateObject);
       if (guiObjString) {
-        const guiObj = JSON.parse(guiObjString)
-        gui.load(guiObj)
+        const guiObj = JSON.parse(guiObjString);
+        gui.load(guiObj);
 
         // Reset saved state
         const guiParams = {
           resetState: () => {
-            localStorage.removeItem(guiStateObject)
+            localStorage.removeItem(guiStateObject);
 
-            location.reload()
+            location.reload();
           },
-        }
-        gui.add(guiParams, 'resetState')
+        };
+        gui.add(guiParams, "resetState");
       }
     }
-  }, [])
+  }, []);
 
   return (
     <div className="App">
       <h1>
-        Complementary Colors with <code>Chroma.js</code>
+        Complementary Colors with <code>color2k</code>
       </h1>
       <div className="colors-container">
         <ColorSpot color={primaryColor} />
-        <ColorSpot color={complementaryColor} />
+        <ColorSpot color={toHex(complementaryColor)} />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
